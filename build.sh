96e92b1a46fdee965006dfce6633cd867c89efb9
Abdulameen <ifeoluwaabdulameen@gmail.com>
2023-05-07 00:13:02 +0100

    try

--- build.sh
+++ build.sh
@@ -11,4 +11,8 @@ echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput
 
 echo "Running Celery Tasks..."
+cd /vercel/path0 || exit 1
+#Activate virtual env
+. . venv/bin/activate
+
 celery -A football worker -l info
